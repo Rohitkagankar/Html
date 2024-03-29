146aca50aaf26bde0dd9ff0bf24ad2a0cfecbb85
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-03-29 23:22:12 +0530

    mouse events

--- jquery/script.js
+++ jquery/script.js
@@ -3,10 +3,29 @@ $("button").addClass("btn");
 $("button").text("rohit");
 $("button").html("<em>hello world</em>");
 
-// $("button").click(function(){
-//     $("h2").text("thank you...");
-// });
+$("button").click(function(){
+    $("h2").text("thank you...");
+});
+
+
+
+//mouse events--------
 $("input").keypress(function(evt){
     console.log(evt.key);
     $("h2").text(evt.key);
-})
+});
+$(".text").mouseenter(function(){
+    $(".text").css("background-color","yellow");
+});
+$(".text").mouseleave(function(){
+    $(".text").css("background-color","orange");
+});
+$(".text").click(function(){
+    $(".text").css("background-color","black");
+});
+$(".text").dblclick(function(){
+    $(".text").css("background-color","violet");
+});
+$(".text").contextmenu(function(){
+    $(".text").css("background-color","pink");
+});
